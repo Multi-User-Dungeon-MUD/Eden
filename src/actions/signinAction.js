03c09011f58a22a4aba701be3379fcd49cd3da9c
Timothy Sharkey <Techne3@gmail.com>
2020-03-03 09:26:12 -0700

    init commit

--- src/actions/signinAction.js
+++ src/actions/signinAction.js
@@ -7,7 +7,7 @@ export const LOGIN_FAILURE = "LOGIN_FAILURE";
 export const fetchLogin = (account, history) => dispatch => {
   dispatch({ type: LOGIN_FETCH });
   axios
-    .post(`/api/adv/login`, account)
+    .post(`d/api/adv/login`, account)
     .then(res => {
       dispatch({ type: LOGIN_SUCCESS, payload: res.data });
       history.push("/dashboard");
